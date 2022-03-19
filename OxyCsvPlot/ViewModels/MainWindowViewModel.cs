7518967a44b49e2ea9a0b3ea1f8e45811d07e9e2
t13801206 <t13801206@yahoo.co.jp>
2022-03-19 20:23:56 +0900

    add mew project

---
+++ OxyCsvPlot/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Prism.Mvvm;
+
+namespace OxyCsvPlot.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
