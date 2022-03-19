a25cf52670273c925b980e6f3eb224344a8dda17
t13801206 <t13801206@yahoo.co.jp>
2022-03-19 16:05:25 +0900

    プロジェクト ファイルを追加します。

---
+++ HelloOxyPlot/MainWindowViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using OxyPlot;
+using OxyPlot.Series;
+using Reactive.Bindings;
+
+namespace HelloOxyPlot
+{
+    public class MainWindowViewModel
+    {
+        public PlotModel MyModel1 { get; }
+        public PlotModel MyModel2 { get; }
+        public PlotModel MyModel3 { get; }
+
+        public ReactiveCommand UpdateGraphCommand1 { get; }
+        public ReactiveCommand UpdateGraphCommand2 { get; }
+        public ReactiveCommand UpdateGraphCommand3 { get; }
+
+        private readonly LineSeries _lineSeries = new LineSeries();
+        private readonly PlotModelHandler _plotModelHandler = new PlotModelHandler();
+
+        public MainWindowViewModel()
+        {
+            MyModel1 = new PlotModel { Title = "Example 1" };
+            MyModel1.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
+
+            MyModel2 = new PlotModel();
+            MyModel2.Series.Add(_lineSeries);
+
+            MyModel3 = _plotModelHandler.Create();
+
+            UpdateGraphCommand1 = new ReactiveCommand().WithSubscribe(UpdateGraph1);
+            UpdateGraphCommand2 = new ReactiveCommand().WithSubscribe(UpdateGraph2);
+            UpdateGraphCommand3 = new ReactiveCommand().WithSubscribe(UpdateGraph3);
+        }
+
+        private void UpdateGraph1()
+        {
+            MyModel1.Series.Clear();
+            MyModel1.Series.Add(new FunctionSeries(Math.Sin, 0, 10, 0.1, "sin(x)"));
+            MyModel1.InvalidatePlot(true);
+        }
+
+        private void UpdateGraph2()
+        {
+            var x = DateTime.Now.Second;
+            var y = new Random().Next(-100, 100);
+
+            _lineSeries.Points.Add(new DataPoint(x, y));
+
+            MyModel2.InvalidatePlot(true);
+        }
+
+        private void UpdateGraph3() => _plotModelHandler.Update();
+    }
+}
---
+++ HelloOxyPlot/PlotModelHandler.cs
@@ -0,0 +1,60 @@
+﻿using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using System;
+
+namespace HelloOxyPlot
+{
+    internal class PlotModelHandler
+    {
+        private readonly PlotModel _model;
+        private readonly LineSeries _series;
+        
+        internal PlotModelHandler()
+        {
+            var XAxis = new LinearAxis
+            {
+                Position = AxisPosition.Bottom,
+                TickStyle = TickStyle.Inside,
+                Maximum = 1024,
+                MajorStep = 256,
+            };
+
+            var YAxis = new LinearAxis
+            {
+                Position = AxisPosition.Left,
+                TickStyle = TickStyle.Inside,
+                Maximum = 4096,
+                MajorStep = 1024,
+            };
+
+            _series = new LineSeries
+            {
+                Title = "Line",
+                InterpolationAlgorithm = InterpolationAlgorithms.UniformCatmullRomSpline,//グラフの角を丸める
+                Color = OxyColor.FromArgb(0xff, 0xff, 0, 0),     // 上の線の色
+                StrokeThickness = 2,                             // 線の太さ
+            };
+
+            // 点を追加
+            _series.Points.Add(new DataPoint(10, 123.4));
+
+            _model = new PlotModel();
+            _model.Title = "グラフのタイトル";
+            _model.Axes.Add(XAxis);
+            _model.Axes.Add(YAxis);
+            _model.Series.Add(_series);
+        }
+
+        internal PlotModel Create() => _model;
+
+        internal void Update()
+        {
+            var x = new Random().Next(0, 1023);
+            var y = new Random().Next(0, 4065);
+            _series.Points.Add(new DataPoint(x, y));
+
+            _model.InvalidatePlot(true);
+        }
+    }
+}
