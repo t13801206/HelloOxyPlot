77c76db4a624b487354e6013083ec501700ae0ad
t13801206 <t13801206@yahoo.co.jp>
2022-03-21 17:25:58 +0900

    it works

---
+++ OxyCsvPlot/Models/CsvDataModel.cs
@@ -0,0 +1,26 @@
+﻿using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OxyCsvPlot.Models
+{
+    internal class CsvDataModel
+    {
+        public DateTime DateTime { get; set; }
+        public double Data1 { get; set; }
+        public double Data2 { get; set; }
+        public double Data3 { get; set; }
+    }
+
+    internal sealed class CsvDataMap : ClassMap<CsvDataModel>
+    {
+        public CsvDataMap()
+        {
+            Map(x => x.DateTime).Index(0);
+            Map(x => x.Data1).Index(1);
+            Map(x => x.Data2).Index(2);
+            Map(x => x.Data3).Index(3);
+        }
+    }
+}
---
+++ OxyCsvPlot/Models/CsvHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace OxyCsvPlot.Models
+{
+    public class CsvHandler
+    {
+        private readonly string _csvPath;
+
+        public CsvHandler(string csvPath)
+        {
+            _csvPath = csvPath;
+
+            FileCheck();
+        }
+
+        public CsvReader OpenCsvReader()
+        {
+            var stream = new StreamReader(_csvPath, Encoding.UTF8);
+            var cultureInfo = new CultureInfo("ja-JP");
+            var reader = new CsvReader(stream, cultureInfo);
+
+            return reader;
+        }
+        
+        private void FileCheck()
+        {
+            if (!File.Exists(_csvPath))
+                throw new FileNotFoundException($"File Not Found!! --->>> {_csvPath}");
+
+            if (!_csvPath.EndsWith(".csv", StringComparison.CurrentCultureIgnoreCase))
+                throw new FormatException($"Invalid Extension!! --->>> {_csvPath}");
+        }
+    }
+}
